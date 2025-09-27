d6b291ef405c8d0faeb235687b361900afeec788
KhangPC <khangit1304@gmail.com>
2025-09-28 00:46:53 +0700

    update logo -front

--- cli/src/app/app.config.ts
+++ cli/src/app/app.config.ts
@@ -1,6 +1,11 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
-import { provideHttpClient, withInterceptorsFromDi, withFetch, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { provideRouter, withInMemoryScrolling } from '@angular/router';
+import {
+  provideHttpClient,
+  withInterceptorsFromDi,
+  withFetch,
+  HTTP_INTERCEPTORS,
+} from '@angular/common/http';
 
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
@@ -9,13 +14,19 @@ import { AuthInterceptor } from './core/interceptors/auth.interceptor';
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes),
     provideClientHydration(),
     provideHttpClient(withInterceptorsFromDi(), withFetch()),
     {
       provide: HTTP_INTERCEPTORS,
       useClass: AuthInterceptor,
-      multi: true
-    }
-  ]
+      multi: true,
+    },
+    provideRouter(
+      routes,
+      withInMemoryScrolling({
+        scrollPositionRestoration: 'enabled',
+        anchorScrolling: 'enabled',
+      })
+    ),
+  ],
 };
